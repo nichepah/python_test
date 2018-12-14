6b38b246f524f044d012aa87e488fa26d6ac9f5c
nichepah <nichepah@gmail.com>
2018-12-14 12:28:48 +0530

    Delete 24_list_comprension.py

--- 24_list_comprension.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    """ Demo List comprehension.
-    
-    Create a list of cubes of numbers from 1 to 100
-    Uses a for loop for definition
-    """
-    cubes = [ x**3 for x in range(1, 10)]
-    print(cubes)
