7588c2132fa8535d17316d157e2e037d438cd63a
nichepah <nichepah@gmail.com>
2018-12-15 18:36:44 +0530

    matrix reloaded

--- 32_matrix.py
+++ 32_matrix.py
@@ -1,29 +1,55 @@
+import numpy as np
+import sys
+
 if __name__ == "__main__":
     """ Implement matrix.
 
-    Twi ways of using matrix.
-    1. Use standard lists
+    Two ways of getting matrix.
+    1. Use standard lists (Note : Implement transpose and multiplication yourself ;-))
     2. Use numpy
     Move to numpy as it contains many more features for math
     """
-    roll_no = {'masha': 12, 'shopkins': 23, 'maya': 2}
-    # unordered
-    print(roll_no)
-    # key as index
-    print(roll_no['masha'])
-    # list keys
-    print(list(roll_no.keys()))
-    # sorted list of keys
-    print(sorted(list(roll_no.keys())))
-    # membership as in set
-    print("'masha' in roll_no", 'masha' in roll_no)
-    print("'bear' in roll_no", 'bear' in roll_no)
-    # dict comprehension
-    cube = {x: x ** 3 for x in range(20)}
-    print(cube)
-    mobile_no = dict(aneesh=8986880000, natasha=8986880432, angela=89868804300)
-    print(mobile_no)
-    print("looping")
-    for i, v in enumerate(roll_no):
-        print(i, v)
+    # 3x3 matrix
+    A = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+    print("A : ", A)
+    print("A[1] : ", A[1])
+    # pay attention to the 0-based index of the list
+    print("A[1][2] : ", A[1][2])
+    # len on 2D Matrix works as expected
+    print(len(A))
+    # Add two matrices using nested loop
+
+    B = [[9, 8, 7],
+         [6, 5, 4],
+         [3, 2, 1]]
+
+    R = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+
+    # iterate through rows
+    for i in range(len(A)):
+        # iterate through columns
+        for j in range(len(A[0])):
+            R[i][j] = A[i][j] + B[i][j]
+    for i in R:
+        print(i)
+    # use numpy
+    x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    y = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+    print("numpy element-wise op")
+    print("x+y : \n", np.add(x, y))
+    print("x*y : \n", np.multiply(x, y))
+    print("sqrt(x): \n", np.sqrt(x))
+    print("numpy matrix multiplication")
+    print("np.dot(x,y): \n", np.dot(x, y))
+    print("x.T: \n", x.T)
+    print("np.zeros((2,3)) \n", np.zeros((2,3)))
+    print("np.ones((3,3)) \n", np.ones((3,3)))
+    print("row 1 of A", A[0])
+    print("column 1 of A", A[0, :])
+    # slicing works as expected in numpy arrays
+    print(sys.path)
 
--- 40_module.py
+++ 40_module.py
@@ -1,4 +1,13 @@
 import sys
+# imports askok.py
+# try without if __name__ == "__main__" in askok.py
+import askok
+# import sail.rbu.mti.whoami
+# import sail.rbu.mti.whoami as mt
+# from sail.rbu.rdcis import whoami
+
+
+# since the file name starts with a numeral
 fib = __import__('5_fib')
 fib_append = __import__('6_fib_append')
 
@@ -7,9 +16,19 @@ if __name__ == "__main__":
     """" Import fib and fib_append from 5_fib.py and 6_fib_append.py
     
     dir() shows definitions in the module
+    try other functions and attributes
+    imports askok.py
+    imports from sail.rbu.mti.whoami.py a function whoami
     """
     print(fib.fib(10))
     print(fib_append.fib_append(100))
     print(dir(fib))
     print(dir(sys))
+    askok.ask_ok("is it ok(y/s)")
+    # sail.rbu.mti.whoami.whoami()
+    # mt.whoami()
+    # whoami.whoami()
+
+
+
 
