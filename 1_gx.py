c4154924163a0a541118892cb4d950d1cc45a65f
nichepah <nichepah@gmail.com>
2020-12-13 14:35:43 +0530

    Update 1_gx.py

--- 1_gx.py
+++ 1_gx.py
@@ -1,8 +1,11 @@
 #!/bin/python
-""" Prints g(x) = x/(1-x)^2 """
 
-#range function uses list
-for i in range(10):
-    x = 0.1*i
-    print x, x/(1-x*x)
-
+if __name__ == "__main__":
+    """ This is the main function.
+    
+    Prints g(x) = x/(1-x)^2
+ 
+    """
+    for i in range(10):
+        x = 0.1*i
+        print(x, x/(1-x*x))
