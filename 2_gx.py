0279e2deaba39c3ea4d227f6c82737fc271977eb
nichepah <nichepah@gmail.com>
2020-12-13 14:23:21 +0530

    Update 2_gx.py

--- 2_gx.py
+++ 2_gx.py
@@ -1,13 +1,25 @@
-#!/bin/python
-""" Prints g(x) = x/(1-x)^2, for values i%2==1 """
+# !/bin/python
 
-#range function uses list
+# range function uses list
 def g(x):
-    for i in range(10):
-        x = 0.1*i
-        print x/(1-x*x)
+    """
+    Prints g(x) = x/(1-x)^2, for values i%2==1
 
-for i in range(10):
-    if i%2 == 1:
-        print i
-        print g(0.1*i)
+    :param x: The range
+    :return: Returns float
+    """
+    for i in range(10):
+        x = 0.1 * i
+        print(x / (1 - x * x))
+
+
+if __name__ == "__main__":
+    """
+    The main
+    
+    """
+    for i in range(10):
+        if i % 2 == 1:
+            print(i)
+            print(g(0.1 * i))
+
